ff56e00ad6dabd03bf7b002e007b968c3d8e01c2
Pratik <barvepratik96@gmail.com>
2025-08-04 01:21:26 +0100

    Updates

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 def load_data():
     # Update the path to your CSV as needed
-    return pd.read_csv('/Users/pratik/Github/PyPractice/nifty500_dividends_with_prices.csv')
+    return pd.read_csv('/Users/pratik/Github/dividend-plotly-app/src/data/nifty500_dividends_with_prices.csv')
